0ed0dea5bd0f674d397a7652197d0dda0b272199
jorgoose <contact.jorgensen@gmail.com>
2024-11-21 16:11:59 -0600

    Add ESLint configuration and ignore file for TypeScript project

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+    ],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    env: {
+      node: true,
+      jest: true,
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+    ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+  };
